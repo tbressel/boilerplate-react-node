068468297158de612c99f8d76b6ffbea900765c5
tbressel <tbressel.dev@gmail.com>
2024-03-05 12:52:06 +0100

    Client Implementation : add React router dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
+/////////////////////////////////////
+////////     IMPORTATIONS    ////////
+/////////////////////////////////////
+
+// React library
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Route importation
+import Home from './routes/Home';
+
+
 
 function App() {
   return (
     <>
-    
-  </>
+      <h2>App.tsx</h2>
+
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+</BrowserRouter>
+    </>
   );
 }
 
---
+++ client/src/routes/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <>
+        <h2>Home.tsx</h2>
+        </>
+    )
+}
+
+
+export default Home;
